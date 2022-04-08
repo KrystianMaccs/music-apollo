0017c07cb010a5f24e88512490749f00db771295
Krystian Maccs <krystianmaccs@gmail.com>
2022-04-08 22:06:42 +0100

    fix: crash issue

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -9,7 +9,7 @@ from rest_framework import serializers
 User = get_user_model()
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializers(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
--- apps/users/views.py
+++ apps/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from drf_yasg.utils import swagger_auto_schema
 from django.contrib.auth import get_user_model
-from apps.users.serializers import UserSerializer
+from apps.users.serializers import UserSerializers
 
 
 User = get_user_model()
@@ -11,16 +11,16 @@ User = get_user_model()
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     #permission_classes = [permissions.AllowAny] # Or anon users can't register]
-    serializer_class = UserSerializer
+    serializer_class = UserSerializers
 
 class UpdateUserView(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = UserSerializers
 
 class ListUserView(generics.ListAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = UserSerializers
 
 class DeleteUserView(generics.DestroyAPIView):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = UserSerializers
