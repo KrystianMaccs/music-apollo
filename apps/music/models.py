cfee29fed31644746381353d0514ba15496c15d2
Krystian Maccs <krystianmaccs@gmail.com>
2022-04-15 23:08:40 +0100

    feat: Top collection manager

--- apps/music/models.py
+++ apps/music/models.py
@@ -5,6 +5,10 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class TopCollectionManager(models.Manager):
+    def get_query_set(self):
+        return super(TopCollectionManager, self).get_query_set().filter(availability=1)
+
 class Collection(TimeStampedUUIDModel):
     user = models.OneToOneField(User, on_delete=models.PROTECT, max_length=50)
     genre = models.CharField(max_length=120, blank=False, null=False)
@@ -16,7 +20,7 @@ class Song(TimeStampedUUIDModel):
     track = models.FileField(upload_to="mediafiles/", null=False, blank=False, default=True)
     title = models.CharField(max_length=120, blank=False, null=False)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
-    artiste = models.CharField(max_length=50, null=False, blank=False, default=True)
+    artiste = models.CharField(max_length=50, null=False, blank=False, default="Juice WRLD")
     artwork = models.ImageField(upload_to="staticfiles/", blank=True, default=True)
 
 
