1662c3c46c3754ca771849b817e131b4dd3c2a3a
Krystian Maccs <krystianmaccs@gmail.com>
2022-04-25 22:48:02 +0100

    fix: fixed music serializers

--- apps/music/serializers.py
+++ apps/music/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
+
 from apps.music.models import Collection, Song
+
 
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
-        fields = "__all__"
+        fields = ["user", "genre"]
+
 
 class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
-        fields = "__all__"
+        fields = ["track", "title", "collection", "artiste", "artwork"]
